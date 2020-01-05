de11c8a83b118e2bcbdcf88c28c04ad423cb1d28
Claude Lehmann <claude.lehmann@squibble.me>
2020-01-05 21:15:44 +0100

    Comment out LR decay

--- networks/pairwise_lstm/bilstm_2layer_dropout_plus_2dense.py
+++ networks/pairwise_lstm/bilstm_2layer_dropout_plus_2dense.py
@@ -168,7 +168,7 @@ class bilstm_2layer_dropout(object):
         callbacks.append(ActiveLearningEpochLogger(self.logger, self.epochs))
         callbacks.append(ActiveLearningUncertaintyCallback(self.dataset, self.config, self.logger, self.segment_size, self.spectrogram_height))
         callbacks.append(WandbCallback(save_model=False))
-        callbacks.append(LearningRateScheduler(self.lr_decay_fun(), verbose=1))
+        # callbacks.append(LearningRateScheduler(self.lr_decay_fun(), verbose=1))
         callbacks.append(EarlyStopping(monitor='val_loss', patience=2, verbose=1))
         # callbacks.append(keras.callbacks.CSVLogger(get_experiment_logs(self.network_name + '.csv')))
         # callbacks.append(PlotCallback(self.network_name))
